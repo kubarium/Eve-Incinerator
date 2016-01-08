a8e9eb464d29c0e1c8e8a47349150ba1c34caf5e
Kumsal Obuz <iam@denizkumsal.com>
2016-01-08 10:14:32 -0500

    finishing touches needed

--- index.py
+++ index.py
@@ -4,9 +4,12 @@ import urllib.request
 import re
 import json
 import sqlite3
+from transactions import Transactions
 
 
 app = Flask(__name__)
+
+
 '''
 app.config.update(
     SERVER_NAME="http://localhost:4500"
@@ -16,50 +19,35 @@ app.config.update(
 def hello():
     return render_template('index.html')
 
-@app.route('/am', methods=['POST','GET'])
-def fuck():
+@app.route('/searchByItemName', methods=['POST','GET'])
+def searchByItemName():
+    transactions = Transactions()
+
+    return transactions.searchByItemName(retrieveRequestFor('itemName', request))
+
+@app.route('/cook', methods=['POST','GET'])
+def cook():
+    transactions = Transactions()
     '''
 
+    data = {
+            "package":retrieveRequestFor('ingredients', request).split(","),
+            "len":5
+        }
+    return json.dumps(data)
+    '''
+
+    return transactions.cook(retrieveRequestFor('ingredients', request).split(","))
+
+
+def retrieveRequestFor(variable, request):
     if request.method == 'POST':
-        typeName = request.form['am']
+        return request.form[variable]
     else:
-        typeName = request.args.get('am', '')
-    '''
-
-
-    try:
-        conn = sqlite3.connect("sqlite-latest.sqlite")
-        conn.text_factory =  lambda x: str(x, 'latin1')
-        c = conn.cursor()
-
-        #typeName = request.args.get('am', '')
-
-
-        list = []
-
-        for row in c.execute("SELECT * FROM invTypes WHERE typeName LIKE ?", ("%nano%",)):
-            list.append(row)
-        #rv = c.fetchall()
-
-        data = {
-            "am":"sikmek istiyorum",
-            "sik":list
-        }
-
-        return json.dumps(data)
-
-    except sqlite3.Error as er:
-
-        data = {
-            "yardirsan":"artik",
-            "sik":er
-        }
-
-        return json.dumps(data)
-    #SELECT * FROM invTypes AS types, industryActivityMaterials as materials WHERE materials.materialTypeID = 2869 AND materials.typeID = types.typeID AND types.published = 1 ORDER BY types.typeName;
-
-
+        return request.args.get(variable, '')
 
 if __name__ == "__main__":
-    #fuck()
+
+    #Transactions().searchByItemName("nano")
+    #Transactions().cook([2869])
     app.run(debug=True, use_reloader=True)
--- transactions.py
+++ transactions.py
@@ -1,11 +1,57 @@
 import sqlite3
+import json
 from flask import g
 
-DATABASE = 'sqlite-latest.sqlite'
-
 class Transactions:
-    def __init__(self):
-        pass
+
+
+    def __init__(self, database = "sqlite-latest.sqlite"):
+
+        self.conn = sqlite3.connect(database)
+        self.conn.text_factory = lambda x: str(x, 'latin1')
+        self.cur = self.conn.cursor()
+
 
     def searchByItemName(self, name):
-        pass
+        cur = self.cur
+
+        cur.execute("SELECT typeID as id, typeName as name, description FROM invTypes WHERE typeName LIKE ? AND published = 1 ORDER BY typeID", ("%"+name+"%",))
+
+        list = [dict(zip(map(lambda x:x[0], cur.description), row)) for row in cur.fetchall()]
+
+        data = {
+            "package":list
+        }
+
+        return json.dumps(data)
+
+    def cook(self, ingredients):
+        cur = self.cur
+
+        cauldron = []
+
+        for ingredient in ingredients:
+            cur.execute("SELECT typeID FROM industryActivityMaterials WHERE materialTypeID = ? ORDER BY typeID", (ingredient,))
+
+            listOfBlueprints = [row[0] for row in cur.fetchall()]
+
+            cauldron.append(set(listOfBlueprints))
+
+        blueprints = cauldron.pop(0)
+
+        for spit in cauldron:
+            blueprints = blueprints.intersection(spit)
+
+
+        query = "SELECT typeID as id, typeName as name, description FROM invTypes WHERE published=1 AND (" + " OR ".join(("typeID = " + str(blueprint) for blueprint in blueprints)) + ") ORDER BY name"
+        cur.execute(query)
+
+        #typeID IN (?) ORDER BY typeID", (",".join(str(blueprint) for blueprint in blueprints)))
+
+        list = [dict(zip(map(lambda x:x[0], cur.description), row)) for row in cur.fetchall()]
+
+        data = {
+            "package":list
+        }
+
+        return json.dumps(data)
